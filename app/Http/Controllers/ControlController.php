602fcdd7a1d9dc1c9281f2cb5cedad8913dac461
alikuotw <alikuotw@gmail.com>
2017-06-19 18:37:30 +0800

    update switch

--- app/Http/Controllers/ControlController.php
+++ app/Http/Controllers/ControlController.php
@@ -35,12 +35,12 @@ class ControlController extends Controller
                 if( $json['control'][$request-1]['boolean'] == false )
                 {
                     
-                    exec("sudo python3 /var/www/html/real_time/python/switch.py '{$request}' 1");
+                    shell_exec("sudo python3 /var/www/html/real_time/python/switch.py '{$request}' 1");
                     $json['control'][$request-1]['boolean'] = true;
                 }
                 else
                 {
-                    exec("sudo python3 /var/www/html/real_time/python/switch.py '{$request}' 0");
+                    shell_exec("sudo python3 /var/www/html/real_time/python/switch.py '{$request}' 0");
                     $json['control'][$request-1]['boolean'] = false;
                 }
 
--- python/switch.py
+++ python/switch.py
@@ -12,15 +12,14 @@ if sys.platform != 'linux':
         exit()
 
 else:
-        sys.path.insert(0, '/home/pi/workspace/DAESecondStage/')
-import DAE.MySerial as serial
-import DAE.MySQL as sql
-import DAE.Common as common
+        sys.path.insert(0, '/home/fa/workspace/demo')
+
+import dae.serial as serial
+import dae.mysql as sql
+import dae.common as common
 import json
 import datetime
 import time
-
-from openpyxl import Workbook
 
 def main():
     global sqlObj, ser
@@ -42,17 +41,17 @@ def main():
         control(group, switch)
 
     except:
-        common.printError(sys.exc_info())
+        common.log("erorr", sys.exc_info())
 
     finally:
-        common.log('close serial port.')
         ser.close()
 
 def control(group, switch):
+    global ser
     n = 255 if switch == 1 else 0
     group = int(group - 1)
 
-    response = ser.writeCommandToModbus([1, 5, 1, group, n, 0])
+    response = ser.write_command_to_modbus([1, 5, 1, group, n, 0])
     print("Group: {0} is power {1}".format(group, ('on' if switch == 1 else 'off')))
 
 
